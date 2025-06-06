ef51e2ab7e7216e57cf14f03f15518a5da3595f8
Wikki-SV <vigneshsiva1910@gmail.com>
2025-06-06 10:43:55 +0530

    Remove unused data attribute from BlackShirt and WhiteShirt components for cleaner code.

--- src/component/WhiteShirt.tsx
+++ src/component/WhiteShirt.tsx
@@ -296,7 +296,6 @@ export default function WhiteShirt() {
           <section
             key={section.id}
             className="panel min-h-[100vh] lg:min-h-[100dvh] flex items-center justify-center mx-auto lg:max-w-[806px] px-0 md:px-4"
-            data-aos="fade-up"
           >
             <div className="w-full flex flex-col items-start">
               <InfoSection
